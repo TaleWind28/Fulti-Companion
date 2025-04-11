ac131f12f7088d7bf3c3e0350567289f24874121
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-04-11 09:15:28 +0200

    Cambiato posizionamento calendario nella pagina utente ed utilizzzo dei componenti appositi per le funzionalità

--- src/routes/userHomePage/+page.svelte
+++ src/routes/userHomePage/+page.svelte
@@ -3,31 +3,38 @@
     import UserProfile from "../../components/userProfile.svelte";
     import Calendar from "../../components/calendar.svelte";
     import RecentActivityTable from "../../components/recentActivityTable.svelte";
+    import CustomButton from "../../components/customButton.svelte";
+    let hideCalendar = true;
 </script> 
  
-<div class="grid grid-cols-[30%_70%] bg-blue-900"><!--container principale -->
-    <div class=" bg-orange-200"><!-- tab con propic ed informazioni utente -->
-        <div>
-            <UserProfile/>
+<div class="grid grid-cols-[30%_70%]"><!--container principale -->
+    <div class=" bg-orange-200 gap-4"><!-- tab con propic ed informazioni utente -->
+        <div class=" gap-4">
+            <UserProfile/><!-- propic-->
         </div>
-        <div><!-- calendario--> 
-            <div class="flex items-center justify-center">
-                <Calendar/>
-            </div>
+        <br>
+        <div class="flex items-center justify-center gap-4"><!-- prossimo appuntamento--> 
+            <!-- implementare componenete che visualizza il primo appuntamento del calendario e che se premuto abilita la visaulizzazione dello stesso-->
+            <CustomButton text="mostra calendario" dimensions= "w-40 h-10" additionalStyle="hover:bg-blue-600 transition-colors" on:click ={()=>hideCalendar = !hideCalendar}/>
         </div>
-        
-        <div class="flex items-center justify-center"><!-- prossimo appuntamento--> 
-            pino
-        </div>
+        <br>
     </div>
     <!-- tab laterale dx -->
-    <div class=" bg-red-500">
+    <div class=" bg-orange-300">
         <div class=" bg-orange-100"><!-- news tab--> 
             <ShowRelatedNews/>
         </div>
-        <div class=" bg-orange-100 "><!-- attività recenti-->
-            <RecentActivityTable/>
-        </div>
+        {#if hideCalendar}
+            <div class=" bg-orange-100 "><!-- attività recenti-->
+                <RecentActivityTable/>
+            </div>
+        {:else}
+            <div><!-- calendario--> 
+                <div class="flex items-center justify-center">
+                    <Calendar/>
+                </div>
+            </div>
+        {/if}
     </div>
 
 
