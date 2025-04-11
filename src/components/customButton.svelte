8bb3d967d13a9f85763fe3151fef1e01ea9c7805
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-04-11 09:13:51 +0200

    aggiunto additionalStyle per non dover per forza modificare lo stile di default

--- src/components/customButton.svelte
+++ src/components/customButton.svelte
@@ -4,13 +4,14 @@
 
     export let text:string;
     export let style = "bg-blue-500 cursor-pointer rounded text-white"
+    export let additionalStyle = "";
     export let dimensions = "w-24 h-8";
     export let type:'button' | 'submit' | 'reset' = 'button';
     export let icon:IconDefinition | undefined = undefined;
 
 </script>
 
-<button {type} on:click class="{dimensions} {style}">
+<button {type} on:click class="{dimensions} {style} {additionalStyle}">
 {#if icon===undefined}
     {text}
 {:else}
