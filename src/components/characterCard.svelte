0ae6136b80b550dc6a0f8391e441a6a1b85c66d4
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-04-15 15:54:03 +0200

    componente CharacterCard completata senza DB che verrà aggiunta insieme

--- src/components/characterCard.svelte
+++ src/components/characterCard.svelte
@@ -2,7 +2,7 @@
     import { type Character } from "$lib/utility";
     import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
-    import { faArrowAltCircleDown, faCircle, faKhanda, faMagic, faRunning, faShield, faShieldAlt, faShieldBlank, faShieldCat, faShieldHeart, faWandMagic, faWandMagicSparkles } from "@fortawesome/free-solid-svg-icons";
+    import { faBoltLightning, faFire, faHillRockslide, faHorse, faKhanda, faRunning, faShield, faSkullCrossbones, faSnowflake, faSun, faWandMagicSparkles, faWind } from "@fortawesome/free-solid-svg-icons";
     import Fa from "svelte-fa";
 
     export let dimensions ="";
@@ -11,10 +11,10 @@
     export let caracters:Character[] = [{
         name:"Landon McCoy",
         stats:[
-            10,
-            6,
-            6,
-            8
+            12,
+            8,
+            8,
+            6
         ],
         traits:["Duellante Spavaldo e Speranzoso","Giustizia","Diez Coronas"],
         elementalAffinity:[0,1,2,0,1,2,0,1,2],
@@ -24,9 +24,9 @@
     },{
         name:"Chroma Celsius",
         stats:[
-            10,
-            6,
-            6,
+            8,
+            8,
+            8,
             8
         ],
         traits:["Duellante Spavaldo e Speranzoso","Giustizia","Diez Coronas"],
@@ -36,32 +36,39 @@
         pic:"src/images/Logo5.1.png"
     }
     ];
+
+    export let elementalIcons = [ faKhanda,faWind, faBoltLightning, faHorse, faHillRockslide,faFire, faSnowflake,faSun, faSkullCrossbones]
+    export let elemenColor = ["text-gray-400","text-green-500","text-yellow-500","","text-amber-900","text-red-500","text-blue-500","text-cafe_noir-800","text-purple-700"]
 </script>
 
 <br>
 <div class="{dimensions} {padding} flex grid-cols-2 gap-4">
     {#each caracters as car}    
-        <div class="bg-white rounded">
+        <div class= "bg-white rounded">
 
             <!--nome e livello-->
-            <header class="bg-cafe_noir-600 flex items-center justify-between text-white" >
+            <header class="bg-cafe_noir-600 flex items-center justify-around text-white text-2xl" >
                 <div>{car.name}</div>
-                <div>{car.level}</div>
+                <div>LV {car.level}</div>
             </header>
 
             <!-- Character Info-->
-            <div class="flex flex-row rounded gap-2">
+            <div class="flex flex-row gap-2">
                 <!-- lato sx-->
-                <div class="bg-amber-500 flex-col"> 
-                    <img src={car.pic} alt="character pic" class="w-full">
-                    <div>   
-                        healtbar
-                    </div>
-                    <div>   
-                        manabar
-                    </div>
-                    <div>   
-                        inventorybar
+                <div class="bg-white flex-col">
+                    <!-- Avatar --> 
+                    <img src={car.pic} alt="character pic" class=" border border-white w-70 h-70">
+                    <!-- Information Bars-->
+                    <div class="flex  flex-col justify-start items-start">
+                        <div>   
+                            {@render progressiveBar("bg-red-500",60,60)}
+                        </div>
+                        <div>   
+                            {@render progressiveBar("bg-blue-500",60,60)}
+                        </div>
+                        <div>   
+                            {@render progressiveBar("bg-gray-500",60,60)}
+                        </div>
                     </div>
                 </div>
 
@@ -79,7 +86,7 @@
                     </div>
                     <br>
                     <!-- stats -->
-                    <div class="flex items-center justify-around px-4">
+                    <div class="flex items-center justify-around px-4 text-2xl">
                         <p>Des: d{car.stats[0]}</p>
                         <p>Int: d{car.stats[1]}</p>
                         <p>Vig: d{car.stats[2]}</p>
@@ -95,7 +102,7 @@
                     <br>
                     <!-- affinità elementale-->
                     <footer class="flex justify-end items-end">
-                        {@render affinityTable(car)}
+                        {@render affinityTable(car, elementalIcons, elemenColor)}
                     </footer>
                 </div>
             </div>
@@ -103,20 +110,23 @@
     {/each}
 </div>
 
-{#snippet affinityTable(character:Character)}
-    <div class="flex flex-row border">
-        {#each character.elementalAffinity as elem }
-            <div class="border">
-                {#if elem == 0}
-                    none
-                {:else if elem ==1}
+{#snippet affinityTable(character:Character, elements:IconDefinition[], elementsColour:string[])}
+    
+    <div class="grid grid-cols-9 border">
+        {#each character.elementalAffinity as elem, i }
+            <div class="border flex flex-row justify-around">
+                <Fa icon= {elements[i]} class={elementsColour[i]}/>
+            <div>
+                {#if elem == 1}
                     res
-                {:else if elem == 2}
+                {:else if elem ==2}
                     im
                 {/if}
             </div>
+            </div>
         {/each}
     </div>
+
 {/snippet}
 
 {#snippet traits(traits:string[])}
@@ -146,3 +156,9 @@
         <p>{statName}: {stat}</p>
     </div>
 {/snippet}
+
+{#snippet progressiveBar(color:string, actual:number, fixed:number)}
+    <div class="{color} w-70 flex items-center justify-center">
+        {actual}/{fixed}
+    </div>
+{/snippet}  
--- src/routes/characterSheetPage/+page.svelte
+++ src/routes/characterSheetPage/+page.svelte
@@ -1,13 +1,19 @@
 <script>
     import CustomButton from "../../components/customButton.svelte";
     import CharacterCard from "../../components/characterCard.svelte";
+    function createCharacter(){
+
+    }
+    function upload(){
+        
+    }
 </script>
 <div class="bg-cafe_noir-900">
     <br><br><br>
     <div class=" flex items-center justify-center gap-6 bg-white w-fit mx-auto h-20 px-8 rounded">
         <input placeholder="ricerca con il nome personaggio" class=" bg-cafe_noir-600 text-white rounded"/>
-        <CustomButton text="Crea Personaggio" color="bg-cafe_noir-600" dimensions="w-35 h-10"/>
-        <CustomButton text="importa da Json" color="bg-cafe_noir-600" dimensions="w-35 h-10"/>
+        <CustomButton text="Crea Personaggio" color="bg-cafe_noir-600" dimensions="w-35 h-10" on:click{createCharacter}/>
+        <CustomButton text="importa da Json" color="bg-cafe_noir-600" dimensions="w-35 h-10" on:click{upload}/>
     </div>
     <CharacterCard dimensions="w-auto h-auto" padding = "px-8" />
     
