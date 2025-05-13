f1d7132414534af0b04e5eebd62a9d488426b7b7
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-05-13 22:45:39 +0200

    Aggiunta modale che forse funziona

---
+++ src/components/modal.svelte
@@ -0,0 +1,20 @@
+<script>
+	let { showModal = $bindable(), modalText, children } = $props();
+
+	let dialog = $state(); // HTMLDialogElement
+
+	$effect(() => {
+		if (showModal) dialog.showModal();
+	});
+</script>
+
+<dialog 
+	bind:this={dialog}
+	onclose={() => (showModal = false)}
+	onclick={(e) => { if (e.target === dialog) dialog.close(); }}
+>
+	<div>
+		{@render children?.()}
+		<button autofocus onclick={() => dialog.close()}>{modalText}</button>
+	</div>
+</dialog>
