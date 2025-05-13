b41ef91da9e52cf5a9f953a27f3edb1fa37cf5fd
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-05-13 22:44:35 +0200

    Rimosso TEsting

--- src/routes/combatSimulator/+page.svelte
+++ src/routes/combatSimulator/+page.svelte
@@ -1,43 +1,7 @@
 <!-- Combat Simulator -->
-
-
 <script lang="ts">
-    import {Tabs} from "$lib/utility";
-    import RunesTab from "../../components/runesTab.svelte";
-    import Traits from "../../components/traits.svelte";
-    import CustomInput from "../../components/customInput.svelte";
-
-
-    let tabs = new Tabs([{
-        id:1,
-        label:"Statistiche",
-        comp: Traits,
-        props:{
-            identity:"Michele",
-            theme:"vuole",
-            origin:"morire"
-        }
-    }
-    ,{
-        id:2,
-        label:"Classi",
-        comp: CustomInput,
-    }
-    ,{
-        id:3,
-        label:"Incantesimi",
-        comp: CustomInput,
-    },{
-        id:4,
-        label:"Equipaggiamento",
-        comp: CustomInput,
-    },{
-        id:5,
-        label:"Note",
-        comp: CustomInput,
-    }]);
-    
-
 </script>
 
-<RunesTab tabs = {tabs.tabs}></RunesTab>
+
+
+
