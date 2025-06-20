36654920006444dfabd24566c230efdf9ab67a67
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-06-20 14:41:34 +0200

    Pausa Pre Pranzo

--- src/components/charachterComps/statTab.svelte
+++ src/components/charachterComps/statTab.svelte
@@ -2,7 +2,7 @@
     import GeneratorBox from "../Generators/generatorBox.svelte";
 
 //Prima Box
-
+    let characteristics = $state([{name:"DES",value:0},{name:"INT",value:0},{name:"VIG",value:0},{name:"VOL",value:0}]);
 //Seconda Box
 
 //Terza Box
@@ -12,12 +12,36 @@
 //Quinta Box
 
 </script>
-<div>
+<div class="grid grid-cols-2 gap-4">
     <!-- Prima Box: Selettore di Caratteristiche -->
     <GeneratorBox nameTag={"Caratteristiche"}>
-        <section>
-            
-        </section>
+        <div class=" grid grid-cols-2 gap-4 p-2">
+            <!-- Sezione Sx: Selettori di Caratteristiche -->
+            <section>
+                {#each characteristics as char,i}
+                    <div class="flex items-center justify-start">
+                        <p class="w-15 ">{char.name}</p>
+                        <span>
+                            <input type="range" max="12" min="6" step="2" bind:value={char.value} class=" h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer slider">
+                            {#if i === 3}
+                                <!-- Scale markers -->
+                                <footer class="flex justify-between mt-1 px-1">
+                                    {#each [6, 8, 10, 12] as mark}
+                                        <span class="text-xs text-black text-center w-4">d{mark}</span>
+                                    {/each}   
+                                </footer>         
+                            {/if}
+                        </span>
+                    </div>
+                {/each}
+            </section>
+            <!-- Sezione Dx: Spiegazione Spread iniziali -->
+            <section>
+                <div class="bg-cafe_noir-800">
+                    pino
+                </div>
+            </section>
+        </div>
     </GeneratorBox>
 
     
